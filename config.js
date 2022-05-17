d573709c6e819f764ec2c8c56683686ac24e547d
Rissbotz <101449752+Rissbotz@users.noreply.github.com>
2022-05-17 14:22:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['6281256023873'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['6281256023873', 'Call me Riss', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'Riss Botz'
 
 global.multiplier = 69 // The higher, The harder levelup
 
